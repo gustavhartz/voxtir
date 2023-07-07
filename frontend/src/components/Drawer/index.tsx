45df1c3c28bce11c8fb77691e313063f2282b82b
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-07-07 22:10:53 +0200

    feat - add export function

--- frontend/src/components/Drawer/index.tsx
+++ frontend/src/components/Drawer/index.tsx
@@ -7,8 +7,24 @@ import {
   TbFileImport,
 } from 'react-icons/tb';
 import { Tooltip } from 'react-tooltip';
+import { Editor } from '@tiptap/react';
+import { asBlob } from '../../utils/html-to-docx';
+import { saveAs } from 'file-saver';
 
 const Drawer = () => {
+  // Update this function to export the document as a docx file based on the redux context
+  const onExport = async (editor: Editor) => {
+    const data = await asBlob(editor.getHTML(), {
+      orientation: 'portrait',
+    });
+    // if buffer convert to string
+
+    const blob = new Blob([data], {
+      type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    });
+    saveAs(blob, 'voxtir-export.docx');
+  };
+
   return (
     <>
       <aside
