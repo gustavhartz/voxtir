fdca5a31fc8d17381d774b46501aedea1dc10287
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-07-10 21:22:36 +0200

    prettier

--- frontend/src/components/KeyboardSettings/KeySetting.tsx
+++ frontend/src/components/KeyboardSettings/KeySetting.tsx
@@ -11,11 +11,19 @@ import {
   setMute,
 } from '../../state/keyboard';
 
-export type KeyFunction = "playPause" | "skipForward" | "skipBackward" | "playbackUp" | "playbackDown" | "mute" | "volumeUp" | "volumeDown";
+export type KeyFunction =
+  | 'playPause'
+  | 'skipForward'
+  | 'skipBackward'
+  | 'playbackUp'
+  | 'playbackDown'
+  | 'mute'
+  | 'volumeUp'
+  | 'volumeDown';
 
 export interface KeySettingProps {
-    text: string;
-    keyFunction: KeyFunction;
+  text: string;
+  keyFunction: KeyFunction;
 }
 
 const KeySetting: React.FC<KeySettingProps> = (props) => {
@@ -27,72 +35,76 @@ const KeySetting: React.FC<KeySettingProps> = (props) => {
 
   const toggleInput = () => {
     setShowInput(!showInput);
-  }
+  };
   const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const pressedKey = e.key;    
+    const pressedKey = e.key;
     switch (keyFunction) {
-            case "playPause":
-                e.currentTarget.value = pressedKey;
-                dispatch(setPlayPause(e.key));
-                break;
-            case "skipForward":
-                dispatch(setSkipForward(e.key));
-                break;
-            case "skipBackward":
-                dispatch(setSkipBackward(e.key));
-                break;
-            case "playbackUp":
-                dispatch(setPlaybackUp(e.key));
-                break;
-            case "playbackDown":
-                dispatch(setPlaybackDown(e.key));
-                break;
-            case "mute":
-                dispatch(setMute(e.key));
-                break;
-            case "volumeUp":
-                dispatch(setVolumeUp(e.key));
-                break;
-            case "volumeDown":
-                dispatch(setVolumeDown(e.key));
-                break;
-            default:
-                break;
-        }
-    toggleInput();
+      case 'playPause':
+        e.currentTarget.value = pressedKey;
+        dispatch(setPlayPause(e.key));
+        break;
+      case 'skipForward':
+        dispatch(setSkipForward(e.key));
+        break;
+      case 'skipBackward':
+        dispatch(setSkipBackward(e.key));
+        break;
+      case 'playbackUp':
+        dispatch(setPlaybackUp(e.key));
+        break;
+      case 'playbackDown':
+        dispatch(setPlaybackDown(e.key));
+        break;
+      case 'mute':
+        dispatch(setMute(e.key));
+        break;
+      case 'volumeUp':
+        dispatch(setVolumeUp(e.key));
+        break;
+      case 'volumeDown':
+        dispatch(setVolumeDown(e.key));
+        break;
+      default:
+        break;
     }
+    toggleInput();
+  };
 
-    React.useEffect(() => {
-        if (showInput) {
-            inputRef.current?.focus();
-        }
-    }, [showInput])
+  React.useEffect(() => {
+    if (showInput) {
+      inputRef.current?.focus();
+    }
+  }, [showInput]);
   return (
-      <div className="flex flex-row justify-between items-center py-0.5">
-        <h4 className='text-lg text-gray-600 font-medium'>{text}</h4>
-        <div className="items-center flex flex-row space-x-2">
-            <span className="text-2xl">⌘ +</span>
+    <div className="flex flex-row justify-between items-center py-0.5">
+      <h4 className="text-lg text-gray-600 font-medium">{text}</h4>
+      <div className="items-center flex flex-row space-x-2">
+        <span className="text-2xl">⌘ +</span>
 
-            {!showInput && <button
+        {!showInput && (
+          <button
             onClick={toggleInput}
             className="px-2 py-1 min-w-max text-md font-semibold text-black bg-gray-200 hover:animate-pulse hover:duration-1000 rounded-lg"
-            >
+          >
             {keyboardState[keyFunction].toUpperCase()}
-            </button>}
+          </button>
+        )}
 
-            {showInput && <input 
+        {showInput && (
+          <input
             ref={inputRef}
             type="text"
             onKeyUp={handleKeyUp}
             onBlur={toggleInput}
             onAbort={toggleInput}
             value={keyboardState[keyFunction]}
-            className="bg-gray-100 max-w-min" 
+            className="bg-gray-100 max-w-min"
             placeholder="Assign a key..."
-            />}
-        </div>
+          />
+        )}
       </div>
+    </div>
   );
-}
+};
 
 export default KeySetting;
