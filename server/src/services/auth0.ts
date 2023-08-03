b9e5fef75f58a345e39edd1296d00c5e86c75391
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-03 17:36:47 +0200

    feat: static class for auth0 connectivity using a client secret for the management api instead of user token

--- server/src/services/auth0.ts
+++ server/src/services/auth0.ts
@@ -1,34 +1,92 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { Auth0ManagementApiUser } from '../types/auth0';
+import { logger } from '../services/logger.js';
+import jwt from 'jsonwebtoken';
 
 // get environment variables
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const AUT0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUT0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
 
+const baseUrl = `https://${AUTH0_DOMAIN}/`;
+
+interface auth0TokenResponse {
+  access_token: string;
+  token_type: string;
+}
+
+/**
+ * Axios instance for making requests to Auth0 and managing the system token
+ *
+ * @static
+ * @type {AxiosInstance}
+ * @memberof Auth0Client
+ */
 export class Auth0Client {
-  auth0: AxiosInstance;
-  constructor() {
-    this.auth0 = axios.create({
-      baseURL: `https://${AUTH0_DOMAIN}/`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  static auth0: AxiosInstance = axios.create({
+    baseURL: `${baseUrl}`,
+    headers: { 'content-type': 'application/json' },
+  });
+  static systemToken: string = '';
+  static systemTokenExpiration: number = 0;
+
+  static isSystemTokenExpired(): boolean {
+    return Auth0Client.systemTokenExpiration < Date.now();
+  }
+
+  static systemTokenExpiresAt(): string {
+    return new Date(Auth0Client.systemTokenExpiration).toISOString();
+  }
+
+  static async getSystemToken(): Promise<string> {
+    logger.info('Getting system token from Auth0');
+    if (Auth0Client.isSystemTokenExpired()) {
+      logger.info('System token expired, getting new one');
+      try {
+        let tokenResponse = await Auth0Client.auth0.post('oauth/token', {
+          client_id: AUT0_CLIENT_ID,
+          client_secret: AUT0_CLIENT_SECRET,
+          audience: `https://${AUTH0_DOMAIN}/api/v2/`,
+          grant_type: 'client_credentials',
+        });
+
+        let tokenData = tokenResponse.data as auth0TokenResponse;
+        Auth0Client.systemToken = tokenData.access_token;
+
+        let decodedToken = jwt.decode(tokenData.access_token, { json: true });
+        if (!decodedToken || !decodedToken?.exp) {
+          logger.error('Error decoding auth0 system token');
+          throw new Error('Error decoding system token');
+        }
+
+        Auth0Client.systemTokenExpiration = decodedToken.exp * 1000;
+      } catch (err: any) {
+        console.log(err);
+        logger.error(
+          `Error getting system token from Auth0: ${err.response.status} ${err.response.statusText}`
+        );
+        throw err;
+      }
+    }
+    return Promise.resolve(Auth0Client.systemToken);
   }
 
   /**
    * @throws {Error}
    */
-  async getUserById(
-    userId: string,
-    userToken: String
-  ): Promise<Auth0ManagementApiUser> {
-    const response = await this.auth0.get(`/api/v2/users/${userId}`, {
-      headers: { Authorization: `Bearer ${userToken}` },
-    });
-    if (response.status !== 200) {
-      throw new Error('Error getting user');
-    } else {
+  static async getUserById(userId: string): Promise<Auth0ManagementApiUser> {
+    logger.info(`Getting user ${userId} from Auth0`);
+    try {
+      let systemToken = await Auth0Client.getSystemToken();
+      const response = await Auth0Client.auth0.get(`/api/v2/users/${userId}`, {
+        headers: { Authorization: `Bearer ${systemToken}` },
+      });
       return response.data as Auth0ManagementApiUser;
+    } catch (err: any) {
+      logger.warn(
+        `Error getting user ${userId} from Auth0: ${err.response.data} ${err.response.status} ${err.response.statusText}`
+      );
+      throw new Error('Error getting user from Auth0');
     }
   }
 }
@@ -36,10 +94,8 @@ export class Auth0Client {
 let isRunningDirectly = false;
 if (isRunningDirectly) {
   // When running the file standalone, you can create an instance of Auth0Client and call its methods here.
-  const auth0Client = new Auth0Client();
-  let fse = await auth0Client.getUserById(
-    'auth0|64c035be0b7eb7c5797264ca',
-    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNLRHdhcjhQa0NQTUJ6bGV2cFB0diJ9.eyJpc3MiOiJodHRwczovL2Rldi1jbXEwNXlvYnR1dmhia3JpLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2NGMwMzViZTBiN2ViN2M1Nzk3MjY0Y2EiLCJhdWQiOlsiaHR0cHM6Ly9kZXYtY21xMDV5b2J0dXZoYmtyaS5ldS5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vZGV2LWNtcTA1eW9idHV2aGJrcmkuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5MDUyNDA1OCwiZXhwIjoxNjkwNjEwNDU4LCJhenAiOiJrTlpvWkNCWlI2MWpialJpTHZCaHZuM3VZRVJTT053RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIifQ.ghZ_99FO2VM-muQukWIvO1XUGJtH4DLCKHSvcZMyHFBCnMwf1Hdrcq9dgwOhpUbLhrnsUlJ3mqANm7Dm_-WbuPMvFyERYpikRSWeYb_584l4K2Y_EXAyuMD2aL6vauL2gYP6QKdKooT0P3Xvhx5OKxW_5Qe2NA-GpflrUhi6K1bcjy27ovyEVOn6Sja4crgAUThuD1KkA_1U2jCwu2WtXVvs7qrC8yZCKUfC9qZyS2aDj52Cm9e-9UsvQ-uaTa2E4s50W9Eigll1sjHdB8lHDAZqJ1rSAyYBvFmRzDwgSFP_SIuCBzjdh8kSq2ES8CcZBAKytbmxMZfTLihIgY4B7A'
-  );
-  console.log(fse);
+  logger.info(Auth0Client.systemTokenExpiresAt());
+  let fse = await Auth0Client.getUserById('auth0|64c035be0b7eb7c5797264ca');
+  logger.info(Auth0Client.systemToken);
+  logger.info(Auth0Client.systemTokenExpiresAt());
 }
