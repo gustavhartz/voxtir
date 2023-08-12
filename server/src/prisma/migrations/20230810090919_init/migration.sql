6b6290e0632c1b3037c832324526007de485992d
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-12 11:47:08 +0200

    fix: convert to new schema

---
+++ server/src/prisma/migrations/20230810090919_init/migration.sql
@@ -0,0 +1,107 @@
+-- CreateEnum
+CREATE TYPE "TranscriptionType" AS ENUM ('AUTOMATIC', 'MANUAL');
+
+-- CreateEnum
+CREATE TYPE "TranscriptProcessingLogicVersion" AS ENUM ('V1');
+
+-- CreateEnum
+CREATE TYPE "TranscriptionProcessStatus" AS ENUM ('CREATED', 'QUEUED', 'PROCESSING', 'FAILED', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "ProjectRole" AS ENUM ('ADMIN', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "data" BYTEA NOT NULL DEFAULT '\x',
+    "audioFileURL" TEXT,
+    "language" TEXT,
+    "speakerCount" INTEGER,
+    "dialect" TEXT,
+    "transcriptionType" "TranscriptionType" NOT NULL,
+    "transcriptionStatus" "TranscriptionProcessStatus",
+    "doSpeakerDiarization" BOOLEAN,
+    "mergedTranscriptionFileURL" TEXT,
+    "speakerDiarizationFileURL" TEXT,
+    "speechToTextFileURL" TEXT,
+    "transcriptionStartedAt" TIMESTAMP(3),
+    "transcriptionFinishedAt" TIMESTAMP(3),
+    "transcriptProcessingLogicVersion" "TranscriptProcessingLogicVersion",
+    "isTrashed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "auth0ManagementApiUserDetails" JSONB,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserOnProject" (
+    "id" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" "ProjectRole" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserOnProject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectInvitation" (
+    "id" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "ProjectRole" NOT NULL,
+    "token" TEXT NOT NULL,
+    "used" BOOLEAN NOT NULL DEFAULT false,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProjectInvitation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Document_projectId_idx" ON "Document"("projectId");
+
+-- CreateIndex
+CREATE INDEX "UserOnProject_projectId_userId_idx" ON "UserOnProject"("projectId", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProjectInvitation_token_key" ON "ProjectInvitation"("token");
+
+-- CreateIndex
+CREATE INDEX "ProjectInvitation_projectId_email_token_idx" ON "ProjectInvitation"("projectId", "email", "token");
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOnProject" ADD CONSTRAINT "UserOnProject_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOnProject" ADD CONSTRAINT "UserOnProject_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectInvitation" ADD CONSTRAINT "ProjectInvitation_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
