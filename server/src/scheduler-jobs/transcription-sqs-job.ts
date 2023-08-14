33ef17ebf368a0b8b1f8ca6b4a05c73ea9efda87
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-14 21:13:48 +0200

    fix: static jobid and better logging

--- server/src/scheduler-jobs/transcription-sqs-job.ts
+++ server/src/scheduler-jobs/transcription-sqs-job.ts
@@ -3,24 +3,25 @@ import {
   pollSqs,
   deleteMessageFromSqsRecivedMessageResult,
 } from '../services/aws-sqs.js';
-import { v4 as uuidv4 } from 'uuid';
-import { logger } from '../services/logger.js';
+import { schedulerLogger as logger } from '../services/logger.js';
 import { SQS_TRANSCRIPTION_QUEUE_URL as QUEUE_URL } from '../common/env.js';
 import { processSQSMessage } from '../transcription/process-event.js';
 
-export const POLL_INTERVAL = 15; // Seconds
+export const POLL_INTERVAL = 10; // Seconds
+export const JOB_ID = 'transcription-sqs-poll';
 
 const task = new AsyncTask(
   'transcription sqs poll and process',
   async () => {
     let res = await pollSqs(QUEUE_URL);
     try {
+      logger.debug(`Received event`, res);
       processSQSMessage(res);
       deleteMessageFromSqsRecivedMessageResult(QUEUE_URL, res);
+      logger.info(`Completed transcription SQS poll`);
     } catch (err) {
-      logger.error('Error processing transcription SQS message. ', err);
+      logger.error('Error processing transcription SQS message', err);
     }
-    logger.info(`Completed transcription SQS poll`);
   },
   (err: Error) => {
     logger.error('Error in transcription SQS processing', err);
@@ -28,5 +29,5 @@ const task = new AsyncTask(
 );
 export const job = new SimpleIntervalJob({ seconds: POLL_INTERVAL }, task, {
   preventOverrun: true,
-  id: uuidv4(),
+  id: JOB_ID,
 });
