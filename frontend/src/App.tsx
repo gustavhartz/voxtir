832d7889f8ed2f1267e87042be7d3b41cb121e6b
MadsFrost <madsanton31@gmail.com>
2023-08-16 20:05:46 +0200

    Setup tanstack table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Me from './pages/Me';
 import KeyboardModal from './components/KeyboardModal';
 import KeyboardSettings from './components/KeyboardSettings';
 import ImportModal from './components/ImportModal';
 import Import from './components/Import';
+import Drawer from './components/Drawer';
 function App() {
   return (
     <div className="w-screen">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/me" element={<Me />} />
+        <Route path="/test" element={<Drawer />} />
       </Routes>
       <KeyboardModal>
         <KeyboardSettings />
