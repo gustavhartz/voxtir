24858c3a12c216519967eb81c23adef78a233dd7
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-19 00:42:34 +0200

    fix: eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,19 +3,47 @@ module.exports = {
     browser: true,
     es2020: true,
   },
+  ignorePatterns: [
+    '**/node_modules/*',
+    '**/dist/*',
+    '**/generated/*',
+    '**/*.local.*',
+    '.eslintrc.cjs',
+    '*config.ts',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
+
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
+  overrides: [
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/known-type-names': 'error',
+      },
+      parserOptions: {
+        schema: './src/routes/graphql/**/*.ts',
+      },
+    },
+  ],
 };
