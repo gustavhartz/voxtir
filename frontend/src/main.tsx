d7399ecadcc0d79c5ad23e53d233de82d68e150a
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-19 01:18:45 +0200

    feat: add auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './graphql/client.ts';
-
-import { store } from './store.ts';
 import './index.css';
 
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.tsx';
+import { Auth0ProviderWithNavigate } from './components/Auth0/auth0-provider-with-navigate.tsx';
+import { client } from './graphql/client.ts';
+import { store } from './store.ts';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </ApolloProvider>
+  <BrowserRouter>
+    <Auth0ProviderWithNavigate>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ApolloProvider>
+    </Auth0ProviderWithNavigate>
+  </BrowserRouter>
 );
