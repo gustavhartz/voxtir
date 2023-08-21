00e2a93f16badda818754f7c627f8257972c916e
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-08-21 23:03:32 +0200

    feat: use logger

--- server/src/routes/hocuspocus.config.ts
+++ server/src/routes/hocuspocus.config.ts
@@ -1,6 +1,7 @@
 import { Database } from '@hocuspocus/extension-database';
 import { Configuration } from '@hocuspocus/server';
 import { v4 as uuidv4 } from 'uuid';
+import { logger } from '../services/logger.js';
 
 // Ripped from
 // docs-plus :) https://github.com/docs-plus/
@@ -12,21 +13,12 @@ export default (): Partial<Configuration> => {
     Partial<Configuration> = {
     name: `${APP_NAME}_${uuidv4().slice(0, 4)}`,
     extensions: [],
-    async onConnect(data) {
-      console.log('onConnect');
-      console.log(data);
-    },
-    // Only works with the token specified from the frontend configuration. Do own logic before handler
-    async onAuthenticate(data) {
-      console.log('onAuthenticate');
-      console.log(data);
-    },
   };
 
   const database = new Database({
     // Return a Promise to retrieve data …
     fetch: async (d) => {
-      console.log('fetch');
+      logger.info('fetch');
       const doc = await prisma.document.findFirst({
         where: {
           id: d.documentName,
@@ -41,7 +33,7 @@ export default (): Partial<Configuration> => {
     },
     // … and a Promise to store data:
     store: async ({ documentName, state }) => {
-      console.log('store');
+      logger.info('store');
       return prisma.document.upsert({
         create: {
           data: state,
