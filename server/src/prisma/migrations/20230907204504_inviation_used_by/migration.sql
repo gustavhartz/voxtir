7fa1edca12d6c4c7c41ce74401dacaba1982963e
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-09-12 12:01:49 +0200

    feat: reset prisma

--- server/src/prisma/migrations/20230907204504_inviation_used_by/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ProjectInvitation" ADD COLUMN     "usedById" TEXT;
-
--- AddForeignKey
-ALTER TABLE "ProjectInvitation" ADD CONSTRAINT "ProjectInvitation_usedById_fkey" FOREIGN KEY ("usedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
