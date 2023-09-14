0d012a9e2bcd2043ffce5553c33982b3ef549c2b
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-09-14 23:09:44 +0200

    fix: pass auth token to API

--- frontend/src/pages/AcceptInvitation.tsx
+++ frontend/src/pages/AcceptInvitation.tsx
@@ -1,43 +1,61 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
 
-import { PageLoader } from "../components/Auth/page-loader";
-import { useAcceptProjectMutation } from "../graphql/generated/graphql";
-const AcceptInvitation = () => {
-    const [searchParams] = useSearchParams();
-    const token = searchParams.get("token") || "";
-    const navigate = useNavigate();
+import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
-    const [acceptProject, { data, called}] = useAcceptProjectMutation({
+import { PageLoader } from '../components/Auth/page-loader';
+import withAccessToken from '../components/Auth/with-access-token';
+import { useAcceptProjectMutation } from '../graphql/generated/graphql';
+const AcceptInvitation = ({ token }: { token: string }): JSX.Element => {
+  const [searchParams] = useSearchParams();
+  const projectInvitationToken = searchParams.get('token');
+  const navigate = useNavigate();
+
+  const [acceptProject, { called }] = useAcceptProjectMutation({
+    context: {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    },
+  });
+
+  React.useEffect(() => {
+    if (projectInvitationToken && !called) {
+      acceptProject({
         variables: {
-            token: token
+          token: projectInvitationToken,
         },
-    });
+      })
+        .then((res) => {
+          if (res.errors) {
+            console.log(res.errors);
+          }
+          toast(res.data?.acceptProjectInvitation.message, {
+            type: 'success',
+            toastId: 'acceptInvitationSuccess',
+            position: 'bottom-right',
+          });
+          navigate('/');
+        })
+        .catch((error) => {
+          toast(error?.message, {
+            type: 'error',
+            toastId: 'acceptInvitationError',
+            position: 'bottom-right',
+          });
+        });
+    }
+  });
 
-    console.log("test")
-
-    console.log(searchParams.get("token"))
-
-    React.useEffect(() => {
-        if (token && !called) {
-            acceptProject({
-                variables: {
-                    token: token
-                }
-            }).then(() => {
-                navigate("/")
-            })
-        }
-    }, [token, called])
-
-    return (
+  return (
     <div>
-        {data?.acceptProjectInvitation.success}
-        {searchParams.get("token")}
-        <PageLoader />
+      <ToastContainer />
+      <PageLoader />
     </div>
-    )
-}
+  );
+};
 
-export default AcceptInvitation;
+const AcceptInvitationWithAccessToken = withAccessToken(AcceptInvitation);
+
+export default AcceptInvitationWithAccessToken;
