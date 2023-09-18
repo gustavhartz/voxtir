f58ee7050b37ed8f310c55ea2360a80ae1960052
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-09-18 18:17:14 +0200

    fix: update migration to new logic

--- server/src/prisma/migrations/20230918160647_init/migration.sql
+++ server/src/prisma/migrations/20230918160647_init/migration.sql
@@ -2,13 +2,13 @@
 CREATE TYPE "TranscriptionType" AS ENUM ('AUTOMATIC', 'MANUAL');
 
 -- CreateEnum
-CREATE TYPE "TranscriptProcessingLogicVersion" AS ENUM ('V1');
-
--- CreateEnum
-CREATE TYPE "TranscriptionProcessStatus" AS ENUM ('CREATED', 'QUEUED', 'PROCESSING', 'FAILED', 'DONE');
+CREATE TYPE "TranscriptionProcessStatus" AS ENUM ('QUEUED', 'TRANSCRIPTION_JOB_RUNNING', 'TRANSCRIPTION_JOB_COMPLETED', 'MERGE_TRANSCRIPTION_DATA_STARTED', 'FAILED', 'DONE');
 
 -- CreateEnum
 CREATE TYPE "ProjectRole" AS ENUM ('ADMIN', 'MEMBER');
+
+-- CreateEnum
+CREATE TYPE "taskType" AS ENUM ('TRANSCRIPTION_JOB_STARTER');
 
 -- CreateTable
 CREATE TABLE "Document" (
@@ -22,18 +22,24 @@ CREATE TABLE "Document" (
     "language" TEXT,
     "speakerCount" INTEGER,
     "dialect" TEXT,
-    "transcriptionType" "TranscriptionType" NOT NULL,
-    "transcriptionStatus" "TranscriptionProcessStatus",
-    "doSpeakerDiarization" BOOLEAN,
-    "mergedTranscriptionFileURL" TEXT,
-    "speakerDiarizationFileURL" TEXT,
-    "speechToTextFileURL" TEXT,
-    "transcriptionStartedAt" TIMESTAMP(3),
-    "transcriptionFinishedAt" TIMESTAMP(3),
-    "transcriptProcessingLogicVersion" "TranscriptProcessingLogicVersion",
     "isTrashed" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TranscriptionJob" (
+    "id" TEXT NOT NULL,
+    "documentId" TEXT NOT NULL,
+    "jobName" TEXT,
+    "jobStartedAt" TIMESTAMP(3),
+    "jobFinishedAt" TIMESTAMP(3),
+    "type" "TranscriptionType" NOT NULL,
+    "status" "TranscriptionProcessStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TranscriptionJob_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -95,8 +101,27 @@ CREATE TABLE "PinnedProjects" (
     CONSTRAINT "PinnedProjects_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "type" "taskType" NOT NULL,
+    "details" TEXT NOT NULL,
+    "isLocked" BOOLEAN NOT NULL,
+    "lastSuccessAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE INDEX "Document_projectId_idx" ON "Document"("projectId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TranscriptionJob_documentId_key" ON "TranscriptionJob"("documentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TranscriptionJob_jobName_key" ON "TranscriptionJob"("jobName");
 
 -- CreateIndex
 CREATE INDEX "UserOnProject_projectId_userId_idx" ON "UserOnProject"("projectId", "userId");
@@ -113,8 +138,14 @@ CREATE INDEX "PinnedProjects_projectId_userId_idx" ON "PinnedProjects"("projectI
 -- CreateIndex
 CREATE UNIQUE INDEX "PinnedProjects_projectId_userId_key" ON "PinnedProjects"("projectId", "userId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_type_key" ON "Task"("type");
+
 -- AddForeignKey
 ALTER TABLE "Document" ADD CONSTRAINT "Document_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TranscriptionJob" ADD CONSTRAINT "TranscriptionJob_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "UserOnProject" ADD CONSTRAINT "UserOnProject_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -133,3 +164,6 @@ ALTER TABLE "PinnedProjects" ADD CONSTRAINT "PinnedProjects_projectId_fkey" FORE
 
 -- AddForeignKey
 ALTER TABLE "PinnedProjects" ADD CONSTRAINT "PinnedProjects_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- DATA Entry 
+INSERT INTO "Task" ("type", "details", "isLocked", "lastSuccessAt", "createdAt", "updatedAt") VALUES ('TRANSCRIPTION_JOB_STARTER', 'Used to enforce that only one job runs at a time', false, null, '2023-09-18 00:00:00', '2023-09-18 00:00:00');
