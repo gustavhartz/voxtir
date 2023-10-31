a46cb6d82552ab161632c106950376d523ddf491
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2023-10-31 22:39:46 +0100

    fix: remove gql upload

--- server/src/routes/graphql/resolvers/index.ts
+++ server/src/routes/graphql/resolvers/index.ts
@@ -1,12 +1,8 @@
-import { GraphQLUpload } from 'graphql-upload-minimal';
-
 import { Resolvers } from '../generated/graphql';
 import mutations from './mutations.js';
 import queries from './queries.js';
 
 export const resolvers: Resolvers = {
-  Upload: GraphQLUpload,
-
   Query: {
     ...queries,
   },
